b3571b20f3bd03bac646a4e9c90a703644711a9f
OArreola <orlando_arreola@hotmail.com>
2020-02-27 21:31:02 -0800

    Using single element to listent to all buttons

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,74 @@
+// //Using separate event listener for each button
+// function init() {
+//   let $ = function(id) {
+//     return window.document.getElementById(id);
+//   };
+
+//   function enter(val) {
+//     $("result").value += val;
+//   }
+
+//   function calculate() {
+//     let strResult = $("result").value;
+//     $("result").value = eval(strResult);
+//   }
+
+//   $("seven").addEventListener("click", function() {
+//     enter($("seven").value);
+//   });
+//   $("eight").addEventListener("click", function() {
+//     enter($("eight").value);
+//   });
+//   $("nine").addEventListener("click", function() {
+//     enter($("nine").value);
+//   });
+//   $("divide").addEventListener("click", function() {
+//     enter($("divide").value);
+//   });
+
+//   $("four").addEventListener("click", function() {
+//     enter($("four").value);
+//   });
+//   $("five").addEventListener("click", function() {
+//     enter($("five").value);
+//   });
+//   $("six").addEventListener("click", function() {
+//     enter($("six").value);
+//   });
+//   $("multiply").addEventListener("click", function() {
+//     enter($("multiply").value);
+//   });
+
+//   $("one").addEventListener("click", function() {
+//     enter($("one").value);
+//   });
+//   $("two").addEventListener("click", function() {
+//     enter($("two").value);
+//   });
+//   $("three").addEventListener("click", function() {
+//     enter($("three").value);
+//   });
+//   $("subtract").addEventListener("click", function() {
+//     enter($("subtract").value);
+//   });
+
+//   $("zero").addEventListener("click", function() {
+//     enter($("zero").value);
+//   });
+//   $("period").addEventListener("click", function() {
+//     enter($("period").value);
+//   });
+//   $("add").addEventListener("click", function() {
+//     enter($("add").value);
+//   });
+//   $("equal").addEventListener("click", function() {
+//     calculate();
+//   });
+// }
+
+// window.addEventListener("load", init, false);
+
+//Using one event listener for all buttons
 function init() {
   let $ = function(id) {
     return window.document.getElementById(id);
@@ -12,56 +83,13 @@ function init() {
     $("result").value = eval(strResult);
   }
 
-  $("seven").addEventListener("click", function() {
-    enter($("seven").value);
-  });
-  $("eight").addEventListener("click", function() {
-    enter($("eight").value);
-  });
-  $("nine").addEventListener("click", function() {
-    enter($("nine").value);
-  });
-  $("divide").addEventListener("click", function() {
-    enter($("divide").value);
-  });
-
-  $("four").addEventListener("click", function() {
-    enter($("four").value);
-  });
-  $("five").addEventListener("click", function() {
-    enter($("five").value);
-  });
-  $("six").addEventListener("click", function() {
-    enter($("six").value);
-  });
-  $("multiply").addEventListener("click", function() {
-    enter($("multiply").value);
-  });
-
-  $("one").addEventListener("click", function() {
-    enter($("one").value);
-  });
-  $("two").addEventListener("click", function() {
-    enter($("two").value);
-  });
-  $("three").addEventListener("click", function() {
-    enter($("three").value);
-  });
-  $("subtract").addEventListener("click", function() {
-    enter($("subtract").value);
-  });
-
-  $("zero").addEventListener("click", function() {
-    enter($("zero").value);
-  });
-  $("period").addEventListener("click", function() {
-    enter($("period").value);
-  });
-  $("add").addEventListener("click", function() {
-    enter($("add").value);
-  });
-  $("equal").addEventListener("click", function() {
-    calculate();
+  $("buttons").addEventListener("click", function(e) {
+    let btn = e.target.value;
+    if (btn === "=") {
+      calculate();
+    } else {
+      enter(btn);
+    }
   });
 }
 
